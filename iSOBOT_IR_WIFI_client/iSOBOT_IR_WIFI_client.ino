e53e816afd95f3ffd21163e8ca37c6f1dc35056f
Chip <imchipwood@gmail.com>
2017-10-25 17:57:18 -0700

    changed second isobot ip to 192.168.4.2

--- iSOBOT_IR_WIFI_client/iSOBOT_IR_WIFI_client.ino
+++ iSOBOT_IR_WIFI_client/iSOBOT_IR_WIFI_client.ino
@@ -34,7 +34,16 @@ const char* password = "psuisobot";
 WiFiServer server(80);
 WiFiClient client;
 
-IPAddress ip(192, 168, 4, 4);
+/* Wireless connection settings
+ *  ip - this is the IP address for THIS ESP8266
+ *  gateway - the IP address of the ESP8266 configured as WIFI_AP_STA (access point + station)
+ *  subnet - the subnet to use on the wireless network - don't change unless you know what you're doing
+ *  
+ *  If using the iSOBOT_IR_WIFI_AP.ino sketch for the ESP8266 acting as the access point, you can connect
+ *  as many ESP8266s to the network as you like simply by changing the fourth # in the 'ip' variable below.
+ *  Each ESP8266 will need a unique IP address so every time you upload to a new ESP8266, increment the IP address by 1
+ */
+IPAddress ip(192, 168, 4, 2);
 IPAddress gateway(192, 168, 4, 1);
 IPAddress subnet(255, 255, 255, 0);
 
